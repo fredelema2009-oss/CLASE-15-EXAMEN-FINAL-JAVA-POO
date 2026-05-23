bb3ab9feea937a2ec6e0d702177ecb40acd83630
Clark2414 <fredelema2009@gmail.com>
2026-05-22 20:54:45 -0500

    Clase15ExamenFinalJavaPOO-Crear la subclase Perro, Construimos el objeto Perro y sobreescribimos el sonido

---
+++ src/Perro.java
@@ -0,0 +1,34 @@
+/*
+CLASE 15: EXAMEN FINAL — PROGRAMACIÓN (JAVA POO)
+Autor: Frederick Lema
+
+Curso: Bootcamp Programación Cero a POO
+Clase: 15 — Examen Final
+
+Fecha: 22 de mayo del 2026
+Tema:Sistema de gestión de mascotas
+
+Enunciado: Sistema de gestión de mascotas
+Se desea desarrollar una aplicación en consola que permita gestionar diferentes
+tipos de mascotas.
+El sistema debe permitir crear mascotas, almacenarlas en una lista, mostrarlas y
+ejecutar acciones propias de cada tipo de mascota.
+Todas las mascotas comparten características generales, pero cada tipo tiene un
+comportamiento diferente.
+*/
+
+// Heredamos de Mascota para representar específicamente a un perro
+public class Perro extends Mascota {
+
+    // Construimos el objeto Perro pasando los datos al constructor padre
+    public Perro(int id, String nombre, int edad) {
+        // Llamamos al constructor de la clase padre 
+        super(id, nombre, edad);
+    }
+
+    // Sobreescribimos el sonido específico que hace un perro
+    @Override
+    public String hacerSonido() {
+        return "¡Guau guau!";
+    }
+}
